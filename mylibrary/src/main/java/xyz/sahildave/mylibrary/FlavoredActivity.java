55247914e0eea9a1ca03c6374ddfde5956c1d5c0
zhoubin <zhoubin@jinrong.net>
2018-04-24 20:23:51 +0800

    ActionBarActivity

---
+++ mylibrary/src/main/java/xyz/sahildave/mylibrary/FlavoredActivity.java
@@ -0,0 +1,7 @@
+package xyz.sahildave.mylibrary;
+
+import android.support.v7.app.ActionBarActivity;
+
+public abstract class FlavoredActivity extends ActionBarActivity {
+    abstract String generateLibraryText();
+}
